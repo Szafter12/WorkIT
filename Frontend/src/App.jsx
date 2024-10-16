e95b902943e6960ca2b6dd35694344051a86f9fb
Jakub <jakubpachut@gmail.com>
2024-10-16 23:20:44 +0200

    create boilerplate

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
 	return (
 		<>
-			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis laboriosam unde voluptate quas dolor, cupiditate ad ab maxime itaque nobis!</p>
 		</>
 	)
 }
