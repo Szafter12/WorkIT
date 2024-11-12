60ec578fb2575d0dd4aebf4fa8116f024798d17c
Jakub <jakubpachut@gmail.com>
2024-11-12 22:01:10 +0100

    setup project

--- Frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-	return (
-		<>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis laboriosam unde voluptate quas dolor, cupiditate ad ab maxime itaque nobis!</p>
-		</>
-	)
-}
-
-export default App
