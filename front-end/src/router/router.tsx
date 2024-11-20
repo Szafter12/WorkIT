03e69add10d7c80cb6568727126f5ff9e03a09dd
Jakub <jakubpachut@gmail.com>
2024-11-20 23:09:49 +0100

    setup react router

---
+++ front-end/src/router/router.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { Layout } from '../components/Layout/Layout'
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+	},
+])
