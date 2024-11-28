1a2a3a4ef0490a12d5d997688c5575bd17b892ef
Jakub <jakubpachut@gmail.com>
2024-11-28 16:17:46 +0100

    change btns color

--- front-end/src/constants/filters.ts
+++ front-end/src/constants/filters.ts
@@ -3,72 +3,68 @@ export const filters = [
 		name: 'Poziom stanowiska',
 		filterContent: [
 			{
-				filter_name: 'Praktykant'
+				filter_name: 'Praktykant',
 			},
 			{
-				filter_name: 'Junior'
+				filter_name: 'Junior',
 			},
 			{
-				filter_name: 'Mid'
+				filter_name: 'Mid',
 			},
 			{
-				filter_name: 'Senior'
+				filter_name: 'Senior',
 			},
 			{
-				filter_name: 'Ekspert'
+				filter_name: 'Ekspert',
 			},
-			
-		]
+		],
 	},
 	{
 		name: 'Rodzaj umowy',
 		filterContent: [
 			{
-				filter_name: 'Umowa o pracę'
+				filter_name: 'Umowa o pracę',
 			},
 			{
-				filter_name: 'Umowa o dzieło'
+				filter_name: 'Umowa o dzieło',
 			},
 			{
-				filter_name: 'Umowa zlecenie'
+				filter_name: 'Umowa zlecenie',
 			},
 			{
-				filter_name: 'Kontrakt B2B'
+				filter_name: 'Kontrakt B2B',
 			},
 			{
-				filter_name: 'Umowa o staż'
+				filter_name: 'Umowa o staż',
 			},
-			
-		]
+		],
 	},
 	{
 		name: 'Wymiar pracy',
 		filterContent: [
 			{
-				filter_name: 'Część etatu'
+				filter_name: 'Część etatu',
 			},
 			{
-				filter_name: 'Dodatkowa / tymczasowa'
+				filter_name: 'Tymczasowa',
 			},
 			{
-				filter_name: 'Pełny etat'
+				filter_name: 'Pełny etat',
 			},
-			
-		]
+		],
 	},
 	{
 		name: 'Tryb pracy',
 		filterContent: [
 			{
-				filter_name: 'Praca stacjonarna'
+				filter_name: 'Praca stacjonarna',
 			},
 			{
-				filter_name: 'Praca hybrydowa'
+				filter_name: 'Praca hybrydowa',
 			},
 			{
-				filter_name: 'Praca zdalna'
+				filter_name: 'Praca zdalna',
 			},
-			
-		]
+		],
 	},
 ]
