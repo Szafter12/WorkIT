dff3f35be2bade7d2bb4101f6bd79241b31e853b
Jakub Pachut <jpachut@gmail.com>
2025-11-05 12:36:50 +0100

    adding simple search engine

--- backend/app/Http/Controllers/PostsController.php
+++ backend/app/Http/Controllers/PostsController.php
@@ -10,13 +10,16 @@ class PostsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Posts::orderBy('created_at', 'desc')->with(['company', 'level', 'workDimension', 'workMode', 'contractType', 'jobRequirements', 'jobResponsibilities', 'abilities', 'specializations'])->take(25)->get();
+        $query = Posts::orderBy('created_at', 'desc')->with(['company', 'level', 'workDimension', 'workMode', 'contractType', 'jobRequirements', 'jobResponsibilities', 'abilities', 'specializations']);
+
+        $query = $this->search($query, $request);
+        $posts = $query->get();
 
         return response()->json([
             'success' => true,
-            'data' => $posts 
+            'data' => $posts
         ], 200);
     }
 
@@ -33,7 +36,12 @@ class PostsController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $post = Posts::where('id', $id)->with(['company', 'level', 'workDimension', 'workMode', 'contractType', 'jobRequirements', 'jobResponsibilities', 'abilities', 'specializations'])->first();
+
+        return response()->json([
+            'success' => true,
+            'data' => $post
+        ], 200);
     }
 
     /**
@@ -50,5 +58,43 @@ class PostsController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+    private function search($query, Request $request)
+    {
+        $searchBar = $request->input('searchBar');
+        $level = $request->query('level');
+        $workDimension = $request->query('workDimension');
+        $workMode = $request->query('workMode');
+        $contractType = $request->query('contractType');
+        $abilities = $request->query('abilities');
+        $specializations = $request->query('specializations');
+
+        if ($searchBar) {
+            $query->where('title', 'like', '%' . $searchBar . '%');
+        }
+        if ($level) {
+            $query->whereIn('level_id', $level);
+        }
+        if ($workDimension) {
+            $query->whereIn('work_dimension_id', $workDimension);
+        }
+        if ($workMode) {
+            $query->whereIn('work_mode_id', $workMode);
+        }
+        if ($contractType) {
+            $query->whereIn('contract_type_id', $contractType);
+        }
+        if ($abilities) {
+            $query->whereHas('abilities', function ($q) use ($abilities) {
+                $q->whereIn('abilities.id', $abilities);
+            });
+        }
+        if ($specializations) {
+            $query->whereHas('specializations', function ($q) use ($specializations) {
+                $q->whereIn('specializations.id', $specializations);
+            });
+        }
+
+        return $query;
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,3 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("home", [HomeController::class, "index"]);
 Route::get("posts", [PostsController::class, "index"]);
+Route::get("posts/{id}", [PostsController::class, "show"]);
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+    return ['token' => $token->plainTextToken];
+});
+
+Route::get("posts/search", [PostsController::class, "search"]);
